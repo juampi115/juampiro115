46c0de5840d516333ee9437bb1f8cb3ed7d68c80
juampi115 <57188824+juampi115@users.noreply.github.com>
2020-09-20 19:09:19 -0300

    Add files via upload

--- parlante1/parlante1.ino
+++ parlante1/parlante1.ino
@@ -1,27 +1,55 @@
-const int parlante = 3;
-const int pulsador = 2;
-int pul=0;
+//////////////////////// Proyecto Aro de Básquet  /////////////////////
+//////////////////////// Declaración de variables /////////////////////
+int poten = 0;
+int ValMapeado = 0;
+boolean aux = false;
+int spk=3;                             // altavoz a GND y pin 3
+int c[5]={131,262,523,1046,2093};       // frecuencias 4 octavas de Do
+int cs[5]={139,277,554,1108,2217};      // Do#
+int d[5]={147,294,587,1175,2349};       // Re
+int ds[5]={156,311,622,1244,2489};      // Re#
+int e[5]={165,330,659,1319,2637};       // Mi
+int f[5]={175,349,698,1397,2794};       // Fa
+int fs[5]={185,370,740,1480,2960};      // Fa#
+int g[5]={196,392,784,1568,3136};       // Sol
+int gs[5]={208,415,831,1661,3322};      // Sol#
+int a[5]={220,440,880,1760,3520};       // La
+int as[5]={233,466,932,1866,3729};      // La#
+int b[5]={247,494,988,1976,3951};       // Si
 
+
+
+/////////////////////////////////////////////////////////////////////// 
 void setup() {
-  pinMode(pulsador,INPUT);
-  
-  Serial.begin(9600);
- 
+  pinMode(poten,INPUT);
+  attachInterrupt(0, FinDeCarrera, RISING);
+}
+///////////////////////////////////////////////////////////////////////
+void nota(int frec, int t)
+{
+    tone(spk,frec);      // suena la nota frec recibida
+    delay(t);                // para despues de un tiempo t
+}
+///////////////////////////////////////////////////////////////////////
+void FinDeCarrera(){
+  aux = true;
 }
 
+
+///////////////////////////////////////////////////////////////////////
 void loop() {
-  pul= digitalRead(pulsador);
-   Serial.println(pul);
- if (pul == LOW){
-    tone(3,200);
-    delay(500);
+  poten=analogRead(A0);        // Cambio de tono mediante potenciometro
+  ValMapeado=map(poten, 0, 1023, 36, 3000); // Mapeo potenciometro
+ if (aux == false){
+    tone(3,ValMapeado);
+    delay(300);
     noTone(3);
-    delay(500);
+    delay(900);
  }
  else{
-    tone(3,500);
-    delay(200);
-    noTone(3);
-    delay(200);
+     nota(d[3],150);noTone(spk);delay(50);
+     nota(cs[2],150);noTone(spk);delay(50);
+     nota(e[3],700);noTone(spk);delay(1000);
+   aux = false;
  }
 }
